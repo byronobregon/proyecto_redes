0677b3c0e5fa1c5fa64d630d89e63742e35f7862
Byron Obregon <byron.obregon@usach.cl>
2025-06-01 19:43:05 -0400

    Finished servers CRUD

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -18,7 +18,7 @@ class ServersController < ApplicationController
 
     respond_to do |format|
       if @server.save
-        format.html { redirect_to @server, notice: "Server was successfully created." }
+        format.html { redirect_to servers_path, notice: "Servidor Creado" }
         format.json { render :show, status: :created, location: @server }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -30,7 +30,7 @@ class ServersController < ApplicationController
   def update
     respond_to do |format|
       if @server.update(server_params)
-        format.html { redirect_to @server, notice: "Server was successfully updated." }
+        format.html { redirect_to servers_path, notice: "Servidor Actualizado" }
         format.json { render :show, status: :ok, location: @server }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -43,7 +43,7 @@ class ServersController < ApplicationController
     @server.destroy!
 
     respond_to do |format|
-      format.html { redirect_to servers_path, status: :see_other, notice: "Server was successfully destroyed." }
+      format.html { redirect_to servers_path, status: :see_other, notice: "Servidor Eliminado" }
       format.json { head :no_content }
     end
   end
--- app/models/server.rb
+++ app/models/server.rb
@@ -1,3 +1,18 @@
 class Server < ApplicationRecord
   enum :server_status, [:online, :offline, :warning]
+
+  def status_style
+    style = {
+      online: 'bg-green-100 text-green-700',
+      offline: 'bg-red-100 text-red-700',
+      warning: 'bg-yellow-100 text-yellow-700'
+    }
+    style[server_status.to_sym]
+  end
+
+  # TODO: must show progress bars colors
+  # < 50% : bg-green-500
+  # 50% =< 70% : bg-yellow-500
+  # 71% =< 80 : bg-orange-500
+  # > 81% : bg-red-500
 end
